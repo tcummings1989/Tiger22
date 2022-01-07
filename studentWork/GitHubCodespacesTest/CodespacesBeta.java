3105b758a07701231edb6d831deb48a21a59a9d9
tcummings1989 <tcummings@ephrataschools.org>
2022-01-07 18:36:41 +0000

    YoT

---
+++ studentWork/GitHubCodespacesTest/CodespacesBeta.java
@@ -0,0 +1,9 @@
+package studentWork.GitHubCodespacesTest;
+
+public class CodespacesBeta {
+    public static void main(String[] args) {
+        String x = "The year of the Tiger! Let's get scratchin'";
+        System.out.println(x);
+        
+    }
+}
