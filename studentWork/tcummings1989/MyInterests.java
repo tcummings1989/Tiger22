df8ef2dbe5405b9ed935de9853cff6a60e93c3b2
tcummings1989 <tcummings@ephrataschools.org>
2022-02-02 16:55:16 +0000

    c

---
+++ studentWork/tcummings1989/MyInterests.java
@@ -0,0 +1,26 @@
+package studentWork.tcummings1989;
+
+import java.util.ArrayList;
+
+public class MyInterests {
+    
+    public static void main (String[] args){
+
+    ArrayList <String> interests = new ArrayList<String>();
+
+interests.add("Motorcycles"); 
+interests.add("Snowboarding");
+interests.add("Harley Davidson");
+interests.add("Indian");
+interests.add("Honda Civic");
+interests.add("Turbskis");
+interests.add("Shiney Things");
+interests.add("Good Deals");
+
+
+        System.out.println(interests.toString());
+        System.out.println("There are " + interests.size() + " in the list");
+
+
+
+}}
