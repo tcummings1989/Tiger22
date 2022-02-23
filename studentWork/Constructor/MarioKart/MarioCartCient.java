7dfde2280dc8d3f6ef947d753e76fbe809729e52
tcummings1989 <tcummings@ephrataschools.org>
2022-02-23 16:16:37 +0000

    T

---
+++ studentWork/Constructor/MarioKart/MarioCartCient.java
@@ -0,0 +1,14 @@
+package Constructor.MarioKart;
+
+//package studentWork.Constructor.MarioKart;
+
+public class MarioCartCient {
+    public static void main(String[] args) {
+        MarioKartGame defaultGame = new MarioKartGame();
+        System.out.println(defaultGame.getTrack());
+
+        MarioKartGame customGame = new MarioKartGame("Rainbow Road", 40, "easy");
+        System.out.println(customGame.getTrack());
+    }
+    
+}
