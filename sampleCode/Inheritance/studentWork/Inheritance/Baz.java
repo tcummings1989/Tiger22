ea0e451719efccbb8be8cfcd73be7ce71d23eae2
tcummings1989 <tcummings@ephrataschools.org>
2022-03-21 16:17:02 +0000

    t

---
+++ sampleCode/Inheritance/studentWork/Inheritance/Baz.java
@@ -0,0 +1,11 @@
+package studentWork.Inheritance;
+
+public class Baz extends Foo {
+    public String toString() {
+        return "baz";
+    }
+
+    public void method1() {
+        System.out.println("baz 1");
+    }
+}
---
+++ sampleCode/Inheritance/studentWork/Inheritance/Foo.java
@@ -0,0 +1,15 @@
+package studentWork.Inheritance;
+
+public class Foo {
+    public String toString() {
+        return "Foo";
+    }
+
+    public void method1() {
+        System.out.println("foo 1");
+    }
+
+    public void method2() {
+        System.out.println("foo 2");
+    }
+}
