b2eac4435053bd92ef9278bafad7cdd36473f4c4
tcummings1989 <tcummings@ephrataschools.org>
2022-03-23 15:15:44 +0000

    t

---
+++ studentWork/tcummings1989/Inheritance/Animal2/Animal2.java
@@ -0,0 +1,9 @@
+package tcummings1989.Inheritance.Animal2;
+
+class Animal2 { // Superclass (parent)
+    public void animalSound() {
+      System.out.println("The animal makes a sound");
+    }
+  }
+  
+  
---
+++ studentWork/tcummings1989/Inheritance/studentWork/Inheritance/Mumble.java
@@ -0,0 +1,7 @@
+package studentWork.Inheritance;
+
+public class Mumble extends Baz{
+    public void method2() {
+        System.out.println("baz 2");
+    }
+}
