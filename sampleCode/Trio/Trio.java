4ee50305e71526d06da019bf7a592d2779648761
tcummings1989 <tcummings@ephrataschools.org>
2022-04-11 16:12:46 +0000

    T

--- sampleCode/Trio/Trio.java
+++ sampleCode/Trio/Trio.java
@@ -1,2 +1,22 @@
+package studentWork.trio;
+
 public class Trio implements MenuItem {
+
+    private Sandwich sandwich;
+    private Salad salad;
+    private Drink drink;
+
+    public Trio(Sandwich sandwich, Salad salad, Drink drink) {
+        this.sandwich = sandwich;
+        this.salad = salad;
+        this.drink = drink;
+    }
+
+    String getName() {
+       Return = System.out.println(this.sandwich + " / " + this.salad + " / " + this.drink + " Trio");
+    }
+
+    String getPrice(){
+        Return = this.sandwich.getPrice() + this.salad.getPrice() + this.drink.getPrice();
+    }
 }
